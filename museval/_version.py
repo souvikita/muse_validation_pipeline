09ebf48c6c908ccca8a913975d319635e8a8a057
souvikita <sbose925@gmail.com>
2025-05-15 19:38:05 -0700

    fixed mistaken project name in pyproject.toml

--- museval/_version.py
+++ museval/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev20+g82ba2b7.d20250516'
-__version_tuple__ = version_tuple = (0, 1, 'dev20', 'g82ba2b7.d20250516')
+__version__ = version = '0.1.dev21+g007c5c2.d20250516'
+__version_tuple__ = version_tuple = (0, 1, 'dev21', 'g007c5c2.d20250516')
