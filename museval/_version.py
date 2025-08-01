c121c10de0966728cf99c7f840dffb4d27b382b3
Viggo Hansteen <viggoh@gmail.com>
2025-08-01 14:54:00 -0700

    [DEV] Merging

--- museval/_version.py
+++ museval/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev6+g7fdb4eb.d20250510'
-__version_tuple__ = version_tuple = (0, 1, 'dev6', 'g7fdb4eb.d20250510')
+__version__ = version = '0.1.dev12+g0d45074.d20250510'
+__version_tuple__ = version_tuple = (0, 1, 'dev12', 'g0d45074.d20250510')
