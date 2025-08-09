ec6103df035013e93cb014ae7272cde33c34cca0
Souvik Bose <31192984+souvikita@users.noreply.github.com>
2025-08-08 17:10:07 -0700

    fixed multiple things

--- museval/utils.py
+++ museval/utils.py
@@ -1,7 +1,7 @@
 import os
-
+import glob
 def find_response(obs_date, 
-                  resp_dir = os.environ['RESPONSE'], 
+                  resp_dir = None, 
                   delta_month = 12,
                   units = 'DN',
                   verbose = True):
@@ -13,12 +13,20 @@ def find_response(obs_date,
     obs_date: str, format YYYY-MM-DDTH:M:S, eg 2015-07-05T05:16:15
               the obs date will be output as an astropy time object. 
     resp_dir: string, optional, directory where response functions 
-              are stored, default is RESPONSE environment variable.
+              are stored, default is None.
     delta_month: int, optional max number of months away from obs_date before
               new response function is suggested, default 12 months.
     units: str, optional, response function intensity units, default DN.
     verbose: bool, optional, be verbose, default True
     '''
+    from astropy.time import Time
+    from datetime import datetime
+    import numpy as np
+    if resp_dir is None:
+        if 'RESPONSE' not in os.environ:
+            raise EnvironmentError("The environment variable 'RESPONSE' is not set. Set it to the directory where response functions are stored.")
+        resp_dir = os.environ['RESPONSE']
+
     if obs_date is None:
         resp_file = os.path.join(resp_dir, f'aia_resp_{units}.zarr')
         if os.path.exists(resp_file):
@@ -27,7 +35,7 @@ def find_response(obs_date,
             zarr_file = None
     else:
         obs_date = Time(obs_date,format='isot',scale='utc')
-        comp = int(obs_date.strftime("%y"))*12+int(obs_date.strftime("%m"))
+        comp = int(obs_date.datetime.strftime("%y"))*12+int(obs_date.datetime.strftime("%m"))
         resp_files = glob.glob(os.path.join(resp_dir, f'aia_resp_{units}_*.zarr'))
         avail = []
         zarr_file = None
@@ -51,7 +59,7 @@ def get_response(date = None,
                  press = 3e15,
                  dx_pix=0.6, dy_pix=0.6,
                  bands = [94, 131, 171, 193, 211, 304, 335],  
-                 resp_dir = os.environ['RESPONSE'], 
+                 resp_dir = None, 
                  delta_month = 12,
                  ):
     
@@ -74,11 +82,18 @@ def get_response(date = None,
     units: str, optional, response function intensity units, default DN.
     verbose: bool, optional, be verbose, default True
     '''
+    if resp_dir is None:
+        if 'RESPONSE' not in os.environ:
+            raise EnvironmentError("The environment variable 'RESPONSE' is not set. Set it to the directory where response functions are stored.")
+        resp_dir = os.environ['RESPONSE']
+    import aiapy
     import numpy as np
     import xarray as xr
     import astropy.constants as const
     import astropy.units as u
     from aiapy.response import Channel
+    from muse.utils.utils import read_response
+    from muse.synthesis.synthesis import transform_resp_units
     from muse.instr.utils import create_eff_area_xarray
     from muse.instr.utils import chianti_gofnt_linelist
     from muse.instr.utils import create_resp_func, create_resp_line_list, create_resp_func_ci
@@ -98,11 +113,21 @@ def get_response(date = None,
 #
     zarr_file,obs_date = find_response(date, units = units)
     if zarr_file is not None:
-        print(f'*** {zarr_file} already exists! Reading...')
+        print(f'*** {zarr_file} already exists! Reading...') #But it may happen that the same bands are not requested.
     # it is not quite clear how to find the number of gains asked for... should be equal to the number of 
     # lines/bands that the response function was constructed with
         response_all = read_response(zarr_file).compute()
+        if  np.array_equal(bands, response_all.band):
+            print("The bands of the response function match.")
+            return response_all ##this is fine, no need to create a new response function
+        else:
+           print("The bands of the response function do not match the requested bands. Creating a new response function.")
+           need_new_response = True ##Treating this as a flag to create a new response function
+            
+
     else:
+        need_new_response = True
+    if need_new_response:
         logT = xr.DataArray(np.arange(lgtgmin,lgtgmax, lgtgstep),dims='logT')
         pressure = xr.DataArray(np.array([press]), dims= 'pressure')
         print(f"*** Constructing line list")
@@ -114,11 +139,27 @@ def get_response(date = None,
         for band in bands:
             ch = Channel(band*u.angstrom)
             if obs_date is None:
+                #print(f'*** Computing {units} response function for {ch.channel.to_string()}')
+                #response = ch.wavelength_response() * ch.plate_scale
                 print(f'*** Computing {units} response function for {ch.channel.to_string()}')
-                response = ch.wavelength_response() * ch.plate_scale
+                try:
+                    response = ch.wavelength_response() * ch.plate_scale
+                except:
+                    print('*** Warning: correction table taken from local JSOC installation')
+                    response = ch.wavelength_response(correction_table = aiapy.calibrate.util.get_correction_table('JSOC')) * ch.plate_scale
             else:
-                print(f'*** Computing {units} response function for {ch.channel.to_string()} date {obs_date.strftime("%b%Y")}')
-                response = ch.wavelength_response(obstime = obs_date) * ch.plate_scale
+                print(f'*** Computing {units} response function for {ch.channel.to_string()}'
+                       ' date {obs_date.strftime("%b%Y")}')
+                try:
+                    response = ch.wavelength_response(obstime = obs_date, correction_table = aiapy.calibrate.util.get_correction_table('JSOC')) * ch.plate_scale
+                except:
+                    print('*** Warning: correction table taken from local JSOC installation')
+                    response = ch.wavelength_response(obstime = obs_date, correction_table = aiapy.calibrate.util.get_correction_table('JSOC')) \
+                        * ch.plate_scale
+                response = ch.wavelength_response(obstime = obs_date, correction_table = aiapy.calibrate.util.get_correction_table('JSOC')) * ch.plate_scale
+            # else:
+            # print(f'*** Computing {units} response function for {ch.channel.to_string()} date {obs_date.strftime("%b%Y")}')
+            # response = ch.wavelength_response(obstime = obs_date) * ch.plate_scale
             eff_xr = create_eff_area_xarray(response.value, ch.wavelength.value, [ch.channel.value])
             area = eff_xr.eff_area.interp(wavelength=line_list.wvl).fillna(0).drop_vars('wavelength')
             line_list["resp_func"] = line_list.gofnt.sum(['logT']) * area.isel(band=0)
@@ -169,7 +210,7 @@ def get_response(date = None,
         zarr_file = f'aia_resp_{units}_{obs_date.strftime("%b%y")}.zarr'
     zarr_file = os.path.join(os.environ['RESPONSE'],zarr_file)
     if save_response:
-        response_all.to_zarr(zarr_file)
+        response_all.to_zarr(zarr_file, mode='w')
         print(f"Saved response to {zarr_file}  ")
     return response_all
 
