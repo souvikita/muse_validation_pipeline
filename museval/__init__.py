aa091da8d243d7d0fe31553500a3dbc303990676
Viggo Hansteen <viggoh@gmail.com>
2025-08-22 10:16:43 -0700

    [DEV] Added import of itis_synthesis methods

--- museval/__init__.py
+++ museval/__init__.py
@@ -6,3 +6,5 @@ muse_validation_pipeline - museval
 ``museval`` is a library that provides the tools to read in and analyze data from Interface Region Imaging Spectrograph (IRIS).
 """
 from museval.utils import *
+from museval.iris_synthesis import *
+
